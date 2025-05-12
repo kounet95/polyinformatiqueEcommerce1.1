7dcc4ac1fe7e32d74408c667937641cfbcca8bbf
kounet95 <85202452+kounet95@users.noreply.github.com>
2025-05-11 20:09:40 -0400

    vv

--- kounetportfoliocQRS/polyinformatique-core-API/src/main/java/org/example/polyinformatiquecoreapi/commandEcommerce/StartShippingCommand.java
+++
@@ -1,8 +0,0 @@
-package org.example.polyinformatiquecoreapi.commandEcommerce;
-
-public class StartShippingCommand extends BaseCommand<String> {
-    public StartShippingCommand(String id) {
-        super(id);
-    }
-}
-
